fe77e7ea7a1412f75e90bbca817adbde86478bd4
Eyaya <eyayab21@gmail.com>
2024-02-07 22:38:52 +0300

    setting hardhat for the smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,34 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+require('dotenv').config();
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 module.exports = {
-  solidity: "0.4.20",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/LROCBQe_KDc3i3-JHvht-4miTOAI8xDI",
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
+  solidity: {
+    version: "0.4.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,20 +8,20 @@ const hre = require("hardhat");
 
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const unCasinoTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const CasinoedAmount = hre.ethers.parseEther("0.001");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
+  const Casino = await hre.ethers.deployContract("Casino", [unCasinoTime], {
+    value: CasinoedAmount,
   });
 
-  await lock.waitForDeployment();
+  await Casino.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `Casino with ${ethers.formatEther(
+      CasinoedAmount
+    )}ETH and unCasino timestamp ${unCasinoTime} deployed to ${Casino.target}`
   );
 }
 
